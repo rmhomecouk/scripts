6f3e63eba37743ca0dd03f077e790c11dce32bf3
Ricardo Meechan <rico@rmhome.co.uk>
2020-12-02 09:20:22 +0000

    Create check_eth.sh

---
+++ check_eth.sh
@@ -0,0 +1,10 @@
+while(true)
+do
+if ethtool eth1 | grep -q "Speed: 100Mb/s" && ethtool eth1 | grep -q "Duplex: Full" ; then
+#    echo "Online"
+sleep 1
+else
+    echo "Not online"
+ethtool -s eth1 speed 100 duplex full autoneg off
+fi
+done
